69b72c9efe8d7d875ac00b86a9df0070f4f15c16
atuann <trananhhtuan2908@gmail.com>
2025-09-14 21:03:43 +0700

    auto update

---
+++ pages/project_2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(page_title="ATuann", page_icon="👨‍💻", layout="wide")
+st.title("Ghép ảnh skin Liên Quân")
+st.write("Tool ghép ảnh tùy chọn.")
+st.header("COMMING SOON...")
